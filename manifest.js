0c99c5e6b3a011ca2673d7bb1acfc70b981047ab
Lucas Silvestre <lukas.silvestre@gmail.com>
2018-08-08 16:23:45 +0200

    Implement plugin as policy

--- manifest.js
+++ manifest.js
@@ -2,12 +2,26 @@ const plugin = {
   version: '0.1.0',
   policies: ['websocket'],
   init: function (pluginContext) {
-    pluginContext.eventBus.on('http-ready', function ({ httpServer }) {
-      const httpProxy = require('http-proxy')
-      var proxy = new httpProxy.createProxyServer();
-      httpServer.on('upgrade', (req, socket, head) => {
-        proxy.ws(req, socket, head);
-      });
+    pluginContext.registerPolicy({
+      name: 'websocket',
+      policy: (actionParams) => {
+        pluginContext.eventBus.on('http-ready', function ({ httpServer }) {
+          const httpProxy = require('http-proxy')
+          const proxy = new httpProxy.createProxyServer({
+            target: {
+              host: actionParams.host,
+              port: actionParams.port,
+            },
+          });
+          httpServer.on('upgrade', (req, socket, head) => {
+            proxy.ws(req, socket, head);
+          });
+        });
+
+        return (req, res, next) => {
+          return next();
+        }
+      }
     });
   }
 }
